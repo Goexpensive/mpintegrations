a8d8563131bfce71b39c77aa7a93c6eb8b3abd94
Fabio <goexpensive@gmail.com>
2015-04-19 15:46:12 -0300

    csrf exempt

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib import admin
 from .views import NotificationView,NotificationListView
 
 
 urlpatterns = patterns('',
 
-    url(r'^notification/test/$', NotificationView.as_view(), name='notification_test'),
+    url(r'^notification/test/$', csrf_exempt(NotificationView.as_view()), name='notification_test'),
     url(r'^notification/list/$', NotificationListView.as_view(), name='notification_list'),
 
 )
